c51d517000addda0126b93f9f192adec92ac3301
Emily Fernschild <emilyfernschild@gmail.com>
2022-12-20 12:25:37 -0500

    commit tree 71e4e478419bebadc5faffdd2f5d54341f6546f9

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,26 @@
 # your code here
+class Stack  
+     def initialize limit
+         @stack = []
+         @limit = limit
+     end
+   
+    def push(value)
+        if @limit.full?
+            @stack.push(value) 
+        elsif 
+           puts 'Stack Overflow'
+        end   
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+       stack = @stack.to_enum 
+       stack.next
+       stack.next
+       stack.peek
+    end
+end
